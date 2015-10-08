9cc27daa52665a06edf14c65592d7e4a7837c162
Yudi Purwanto <purwantoyudi42@gmail.com>
2015-10-08 20:40:52 +0700

    update routes

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,4 +4,28 @@
  * and open the template in the editor.
  */
 
+(function() {
+	'use strict';
+	angular
+	.module('codeigniterIdApp')
+	.config(function($stateProvider, $urlRouterProvider) {
 
+			// Redirect to the auth state if any other states
+			// are requested other than users
+			$urlRouterProvider.otherwise('/home');
+			
+			$stateProvider
+				.state('home', {
+					url: '/home',
+					templateUrl: 'app/categories/categories.html'
+				})
+				.state('project', {
+					url: '/project',
+					templateUrl: 'app/project/project.html'
+				})
+				.state('categories', {
+					url: '/categories',
+					templateUrl: 'app/categories/categories.html'
+				});
+		});
+})();
