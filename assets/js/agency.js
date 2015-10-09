03f498616f4a1025a9eda34c214a473929452a45
puterakahfi <puterakahfi@gmail.com>
2015-10-10 01:04:02 +0700

    update index.html

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -6,15 +6,18 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function () {
-    /*
-     $('a.page-scroll').bind('click', function(event) {
-     var $anchor = $(this);
-     $('html, body').stop().animate({
-     scrollTop: $($anchor.attr('href')).offset().top
-     }, 1500, 'easeInOutExpo');
-     event.preventDefault();
-     });
-     */
+
+    $('a.page-scroll').on('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('#list-article-button').on('click', function (e) {
+        $('#portfolioModal1').modal('hide');
+    });
 });
 
 // Highlight the top nav as scrolling occurs
