52f4dcf40c33420ac1f2f8ffa233a9d22d562e2b
puterakahfi <puterakahfi@gmail.com>
2015-10-09 11:41:06 +0700

    update module added angular ui animation

--- app/app.module.js
+++ app/app.module.js
@@ -2,5 +2,5 @@
 
 (function () {
     'use strict';
-    angular.module('codeigniterIdApp', ['ngAnimate','ngResource', 'github.activity', 'ui.router']);
+    angular.module('codeigniterIdApp', ['ngAnimate','ngResource', 'github.activity', 'ui.router','anim-in-out']);
 })();
