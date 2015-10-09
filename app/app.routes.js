e487b1fd2f90e318692150d13ffc9373b41d1632
puterakahfi <puterakahfi@gmail.com>
2015-10-09 11:44:01 +0700

    update route

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,27 +4,27 @@
  * and open the template in the editor.
  */
 
-(function() {
-	'use strict';
-	angular
-	.module('codeigniterIdApp')
-	.config(function($stateProvider, $urlRouterProvider) {
+(function () {
+    'use strict';
+    angular
+            .module('codeigniterIdApp')
+            .config(function ($stateProvider, $urlRouterProvider) {
 
-			// Redirect to home
-			$urlRouterProvider.otherwise('/home');
-			
-			$stateProvider
-				.state('home', {
-					url: '/home',
-					templateUrl: 'app/categories/categories.html'
-				})
-				.state('project', {
-					url: '/project',
-					templateUrl: 'app/project/project.html'
-				})
-				.state('categories', {
-					url: '/categories',
-					templateUrl: 'app/categories/categories.html'
-				});
-		});
+                // Redirect to home
+                $urlRouterProvider.otherwise('/home');
+
+                $stateProvider
+                        .state('home', {
+                            url: '/home',
+                            templateUrl: 'app/home/home.html'
+                        })
+                        .state('project', {
+                            url: '/project',
+                            templateUrl: 'app/projects/projects.html'
+                        })
+                        .state('categories', {
+                            url: '/categories',
+                            templateUrl: 'app/categories/categories.html'
+                        });
+            });
 })();
