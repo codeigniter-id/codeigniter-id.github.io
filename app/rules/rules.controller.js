3d59557f3ea94ba593b6e63e2cdc2d13c039fb96
puterakahfi <puterakahfi@gmail.com>
2015-10-09 21:29:29 +0700

    added rules controller

---
+++ app/rules/rules.controller.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// awards.controller.js
+(function () {
+    'use strict';
+    angular.module('codeigniterIdApp')
+            .controller('RulesController', RulesController);
+
+    RulesController.$inject = ['$scope'];
+
+    function RulesController($scope) {
+        
+        $scope.$on('animIn', function () {
+            console.log('Page1Ctrl: animIn');
+        });
+
+        $scope.$on('animOut', function () {
+            console.log('Page1Ctrl: animOut');
+        });
+
+    }
+
+})();
+
