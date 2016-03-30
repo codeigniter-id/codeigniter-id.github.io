b3ca4021e99786cb93a59e5b13177192203b5389
Yudi Purwanto <purwantoyudi42@gmail.com>
2016-03-30 17:14:25 +0700

    guide following john papa

--- app/teams/teams.controller.js
+++ app/teams/teams.controller.js
@@ -1,35 +1,15 @@
-// awards.controller.js
 (function () {
     'use strict';
-    var app = angular.module('codeigniterIdApp');
+    angular.module('codeigniterIdApp')
+            .controller('TeamsController', TeamsController);
 
-/**
-* ups!, sorry john papa we don''t follow your guide this time. :)
-**/
-app.controller('TeamsController', ['$scope','$http', function($scope, $http) {
+    TeamsController.$inject = ['$scope', '$http'];
 
-    $scope.getContributors = getContributors($scope, $http);
-    $scope.http = $http;
-
-
-
-    $scope.getDetailContributor = function getDetailContributor(username){
-       $http({method: 'GET', url: 'https://api.github.com/users/'+ username}).success(function (data) {
-            $scope.details = data; // response data 
-        }).error(function (data) {
-        });
+    function TeamsController($scope, $http) {
+		$http({method: 'GET', url: 'https://api.github.com/repos/codeigniter-id/codeigniter-id.github.io/contributors'}).success(function (data) {
+				$scope.contributors = data; // response data 
+			}).error(function (data) {
+			});
     }
-
-
-}]);
-
-
-function getContributors($scope, $http) {
-    $http({method: 'GET', url: 'https://api.github.com/repos/codeigniter-id/codeigniter-id.github.io/contributors'}).success(function (data) {
-            $scope.contributors = data; // response data 
-        }).error(function (data) {
-        });
-    }
-
-
+	
 })();
