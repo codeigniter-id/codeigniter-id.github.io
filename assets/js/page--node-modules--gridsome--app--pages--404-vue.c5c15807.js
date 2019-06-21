51dbdf0831dd9a7816c262521a171c979b88bd45
puterakahfi <puterakahfi@gmail.com>
2019-06-21 21:03:21 +0700

    Updates

--- assets/js/page--node-modules--gridsome--app--pages--404-vue.c5c15807.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{411:function(n,t,e){"use strict";e.r(t);var s=e(31),u=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("404 - not found")])},[],!1,null,null,null);t.default=u.exports}}]);
